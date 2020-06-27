57a6254711b5a292b0a8b34eaf122b9e83635594
Tanmoy Sen Gupta <36238254+TanmoySG@users.noreply.github.com>
2020-06-27 22:09:45 +0530

    Add files via upload

---
+++ precache-manifest.6840434d16d3174bf8fdd7b58e22b6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2c606efdd636dada643a83edc1323a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b746b581dfa5f9c2225",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b4cb851690d2e17e77d",
+    "url": "/static/js/2.5121cf50.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.5121cf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b746b581dfa5f9c2225",
+    "url": "/static/js/main.c4a080df.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
