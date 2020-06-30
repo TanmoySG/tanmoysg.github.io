eb1fe344d4f8e6ff24a2221b01b3d89c2a101402
Tanmoy Sen Gupta <36238254+TanmoySG@users.noreply.github.com>
2020-07-01 01:53:06 +0530

    0.2 Beta Release
    Added various section to profile.

---
+++ precache-manifest.9881c6af50c583301573a034eebcde95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ceefae27f0a2e01ee2742cd6bcc8713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea4ed29d0f430613484f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9be5b1a88c75d2a5a898",
+    "url": "/static/js/2.e42be606.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.e42be606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4ed29d0f430613484f",
+    "url": "/static/js/main.9fc6c828.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6840434d16d3174bf8fdd7b58e22b6c1.js"
+  "/precache-manifest.9881c6af50c583301573a034eebcde95.js"
 );
 
 self.addEventListener('message', (event) => {
